e7f231605e5c08f2c583cc6997e82e2ee0e80dfc
winargo <rwinargo97@gmail.com>
2018-01-12 23:37:21 +0700

    1/12/2018

---
+++ app/src/main/java/com/fake/shopee/shopeefake/searchresult.java
@@ -0,0 +1,13 @@
+package com.fake.shopee.shopeefake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class searchresult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchresult);
+    }
+}
